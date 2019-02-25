fccb55c39de7703849b00812a3eeb4cb649a817e
Shuchi Jain <shuchijain@zipcoders-MacBook-Pro.local>
2019-02-25 11:38:58 -0500

    Committing blackjack changes

--- src/main/java/io/zipcoder/casino/utilities/CasinoGame/BlackJack.java
+++ src/main/java/io/zipcoder/casino/utilities/CasinoGame/BlackJack.java
@@ -12,15 +12,18 @@ public class BlackJack extends CardGame implements GamblingGame {
 
     Hand playerhd;
     Hand dealerhd;
-    //Hand playerhd = new Hand(new ArrayList<Card>());
-    //Hand dealerhd = new Hand(new ArrayList<Card>());
+    //Hand playerhdlist = new Hand(new ArrayList<Card>()); // For future use in case of Multiple Player
+
     Player dealer;
     Card card;
     int totalPlays = 0;
     int totalWins = 0;
     int playerBet;
+    int playervalue=0;
+    int dealervalue=0;
 
-    public java.util.ArrayList<Integer> betList;
+
+   // public java.util.ArrayList<Integer> betList;   // For future use in Case of multiple Player
     Deck mydeck ;
 
 
@@ -45,38 +48,51 @@ public class BlackJack extends CardGame implements GamblingGame {
 
     public void play() {
 
-        console.println("Welcome to Black Jack!");
+        console.println("WELCOME TO BLACK JACK GAME!!!!!!!! Player" + bjplayer.name);
         bjplayer = new BlackJackPlayer();
 
         //while-loop for playing the game
+
         while (bjplayer.getBalance() > 0) //keep playing as long as the player has money left
         {
 
             //Ask player to place bet
             console.println("You have played " + totalPlays + " times, and won " + totalWins + " times");
-            console.println("You have $" + bjplayer.balance + ", place bet plase.");
+            console.println("You have $" + bjplayer.balance + ", place bet please.");
             playerBet = getBet();
+            playerhd.clearHand();
+            dealerhd.clearHand();
+
 
             if (playerBet > bjplayer.getBalance()) {
                 console.println("You don't have enough money for that.");
+                break;
             }
 
 
-            boolean endRound = false; //variable to keep track of wether current round is going
+            boolean endRound = false; //variable to keep track of weather current round is going
 
 
-            console.println("Dealing the cards... %n");
-
-            mydeck.buildBlackJackDeck();
+            console.println(" Dealer Dealing the cards... %n");
 
 
             //Deal two cards to each player one by one
             for (int i = 0; i < 2; i++) {
-
+                mydeck.buildBlackJackDeck();
 
                 Card card = mydeck.blackJackDeck.pop();
+
                 playerhd.addCard(card);
+                playervalue = playerhd.getBlackJackValue();
+
+
+            }
+            for (int i = 0; i < 2; i++) {
+
+                mydeck.buildBlackJackDeck();
+                Card card = mydeck.blackJackDeck.pop();
                 dealerhd.addCard(card);
+                dealervalue = dealerhd.getBlackJackValue();
 
 
             }
@@ -85,12 +101,14 @@ public class BlackJack extends CardGame implements GamblingGame {
             while (true) //while-loop to keep drawing cards
             {
 
-                console.println("\nYour hand value is currently: "
-                        + playerhd.getBlackJackValue());
+                console.println("\nPlayer hand value is currently: "
+                        + playerhd.value);
+
 
                 //shows the dealers hand, show the first card and hides the others
-                console.println("Dealer hand: " + dealerhd.getBlackJackValue()
+                console.println("Dealer hand: " + dealerhd.getfirstcard()
                         + "[Hidden]");
+
 
                 //Ask if player wants to hit or stand
                 int response = console.getIntegerInput("Do you want to (1)hit or (2)stand?");
@@ -100,51 +118,70 @@ public class BlackJack extends CardGame implements GamblingGame {
                 if (response == 1) {
 
 
-                    Card card = mydeck.blackJackDeck.pop();
-                    playerhd.addCard(card);
+                    Card drawcard = mydeck.blackJackDeck.pop();
+                    playerhd.addCard(drawcard);
+                    mydeck.buildBlackJackDeck();
 
                     console.println("You drew a: "
-                            + card);
+                            + drawcard.getBlackJackEnum());
+
 
                     //If hand value is over 21, bust
                     if (playerhd.getBlackJackValue() > 21) {
-                        console.println("Bust. Your current value is: "
-                                + playerhd.getBlackJackValue());
+                        console.println("You are Busted. Your current value is: "
+                                + playerhd.value);
                         bjplayer.balance -= playerBet; //Withdraw player bet from player bank
+                        playerhd.clearHand();
+                        mydeck.buildBlackJackDeck();
                         endRound = true;
                         break;
                     }
+
+
+                    //Show dealers full hand
+
+                    //Check weather dealers value is higher than players and if game is still going
+                     if ((dealerhd.getBlackJackValue()> playerhd.getBlackJackValue()
+                            && endRound == false)) {
+
+
+                        console.println("Dealers hand Value: " + dealerhd.value);
+                        console.println("Dealer wins!");
+                        bjplayer.balance -= playerBet;
+                        mydeck.buildBlackJackDeck();
+                        endRound = true;
+                    }
+
                 }
                 if (response == 2) //If player stands, end the loop
                 {
-                    // break;
+                    //break;
                     // }
-
-                    //Show dealers full hand
-                    console.println("Dealers hand: " + dealerhd.toString());
-                    //Check weather dealers value is higher than players and if game is still going
-                    if ((dealerhd.getBlackJackValue() > playerhd.getBlackJackValue())
-                            && endRound == false) {
-                        console.println("Dealer wins!");
-                        bjplayer.balance -= playerBet;
-                        endRound = true;
-                    }
                     //dealer-AI, if hand value is 16 or lower, hit.
-                    while ((dealerhd.getBlackJackValue() < 17) && endRound == false) {
-                        mydeck.blackJackDeck.pop();
-                        console.println("Dealer drew: " + dealerhd.toString());
+                    while (dealerhd.value < 17) {
+                        console.println("Dealer hand value is " +dealerhd.value);
+
+                        Card dealerdrawcard = mydeck.blackJackDeck.pop();
+                        console.println("Dealer drew: " + dealerdrawcard.getBlackJackEnum());
+                        dealerhd.addCard(dealerdrawcard);
+                       int afterdrawdealervalue = dealerhd.getBlackJackValue();
+                        console.println("Dealer hand value after draw is " +afterdrawdealervalue);
+                        mydeck.buildBlackJackDeck();
+
 
                     }
 
 
                     //Show the value of dealers hand
-                    console.println("Dealer's hands current value is: " + dealerhd.getSize());
+                    console.println("Dealer's hands current value is: " + dealerhd.value);
                     //If dealer is bust, you win
                     if ((dealerhd.getBlackJackValue() > 21) && endRound == false) {
-                       console.println("Dealer is bust, you win!");
+                        console.println("Dealer is bust, you win!");
                         bjplayer.balance += playerBet;
+                        dealerhd.clearHand();
                         endRound = true;
                         totalWins++;
+                        break;
                     }
                     //check if it's a draw
                     if ((playerhd.getBlackJackValue() == dealerhd.getBlackJackValue()) && endRound == false) {
@@ -157,29 +194,43 @@ public class BlackJack extends CardGame implements GamblingGame {
                     if ((playerhd.getBlackJackValue() > dealerhd.getBlackJackValue()) && endRound == false) {
                         System.out.println("Player win!");
                         bjplayer.balance += playerBet;
-                        endRound = true;
+                        console.println("You Win" + giveWinnings());
+
                         totalWins++;
+                        console.println("No of rounds you won" + totalWins);
+                        endRound = true;
+                        break;
                     } else if (endRound == false) {
                         console.println("You lose the round.");
                         bjplayer.balance -= playerBet;
+                        console.println("Your balance is : " + bjplayer.balance);
                         endRound = true;
+                        break;
                     }
-
-                    System.out.println("End of round.");
-                    totalPlays++;
                 }
 
-                System.out.println("You're out of money, too bad!");
 
-
-
+                System.out.println("End of round.");
+                totalPlays++;
             }
-
-
-            }
-
-
         }
+
+
+
+                 System.out.println("You're out of money, too bad!");
+
+
+            }
+
+
+
+
+
+
+
+
+
+
 
 
         public Integer giveWinnings () {
